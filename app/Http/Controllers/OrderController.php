9d5809d279e618d938097d383cb706b2b0daab77
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-05 23:26:58 +0700

    Feat: Add first name and last name parameters on checkout controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -20,7 +20,7 @@ class OrderController extends Controller
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = config('midtrans.server_key');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isProduction = config('midtrans.is_production');
         // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
@@ -32,7 +32,8 @@ class OrderController extends Controller
                 'gross_amount' => $order->total_price,
             ),
             'customer_details' => array(
-                'name' => $request->name,
+                'first_name' => $request->name,
+                'last_name' => '',
                 'phone' => $request->phone,
             ),
         );
